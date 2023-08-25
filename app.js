7dfabb6a6a8b69bb9deacd5637c84941c2b7183b
Kerim Öztürk <kerimdjozovic97@gmail.com>
2023-08-25 11:43:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req,res) => {
+    res.send('INDEX SAYFASI 2');
+});
+
+
+
+app.listen(PORT,() => {
+    console.log(`Application running port: ${PORT}`);
+})
